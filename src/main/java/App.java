043afef9743dc314d3cbe77e1ceaed67b75869de
Maxim Dobryakov <maxim.dobryakov@gmail.com>
2019-05-05 17:23:26 +0300

    Extract ListViewBitSetSelectionModel from OpenJFX

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,26 +8,30 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import openjfx13.ListViewBitSetSelectionModel;
+
+import java.util.ArrayList;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) {
         int itemsCount = 20_000;
 
-        ObservableList<String> list = FXCollections.observableArrayList();
+        ObservableList<String> list = FXCollections.observableArrayList(new ArrayList<>(itemsCount));
 
         for (int i = 0; i < itemsCount; i++) {
             list.add(String.format("%07d", i));
         }
 
         ListView<String> listView = new ListView<>(list);
+        listView.setSelectionModel(new ListViewBitSetSelectionModel<>(listView));
         listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
         Button button = new Button(String.format("Select %d items and measure selection time", itemsCount));
         button.setOnAction(actionEvent -> {
             long timestamp = System.currentTimeMillis();
 
-            listView.getSelectionModel().selectRange(0, itemsCount - 1); // <== Here `selectRange` take a lot of time to select items
+            listView.getSelectionModel().selectRange(0, itemsCount); // <== Here `selectRange` take a lot of time to select items
 
             String message = String.format("Selection time: %d seconds", (System.currentTimeMillis() - timestamp) / 1000);
             Alert alert = new Alert(Alert.AlertType.INFORMATION, message);
